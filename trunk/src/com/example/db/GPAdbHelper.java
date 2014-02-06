7723389fc4d82816f89ac0c8eb23b214b57febc5
dohyoon.kim131@gmail.com <dohyoon.kim131@gmail.com@56f0695f-1845-8957-950b-df383e6b9fbd>
2014-02-06 06:12:44 +0000

    doyoon  2/6 15:11 com.example.db / com.example.service 패키지 추가

---
+++ trunk/src/com/example/db/GPAdbHelper.java
@@ -0,0 +1,61 @@
+package com.example.db;
+
+/*
+ * created by doyoon
+ * MyGPAdbHelper.java 와 같은 클래스
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class GPAdbHelper extends SQLiteOpenHelper {
+
+	public static final String TABLE_NAME = "gpa_table";
+
+	public static final String ID = "id";
+	public static final String COL_YEAR = "year";
+	public static final String COL_SEMESTER = "semester";
+	public static final String COL_CREDIT = "credit";
+	public static final String COL_GRADE = "grade";
+	public static final String COL_MAJOR = "major";
+	public static final String COL_SUBJECT = "subject";
+
+	private static final String DATABASE_CREATE = GPAdbHelper
+			.dataBaseCreateSQL();
+
+	private static final String DATABASE_NAME = "gpa.db";
+
+	public GPAdbHelper(Context context) {
+		super(context, DATABASE_NAME, null, 1);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+
+		db.execSQL(DATABASE_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static String dataBaseCreateSQL() {
+		StringBuilder sd = new StringBuilder();
+		sd.append("CREATE TABLE ");
+		sd.append(TABLE_NAME);
+		sd.append(" ( ID INTEGER AUTOINCREMENT, ");
+		sd.append(COL_YEAR + " INTEGER,");
+		sd.append(COL_SEMESTER + "INTEGER,");
+		sd.append(COL_CREDIT + "INTEGER,");
+		sd.append(COL_GRADE + "TEXT,");
+		sd.append(COL_MAJOR + "TEXT,");
+		sd.append(COL_SUBJECT + "TEXT");
+		sd.append(");");
+		return sd.toString();
+	}
+
+}
---
+++ trunk/src/com/example/db/GPAdto.java
@@ -0,0 +1,120 @@
+package com.example.db;
+/*
+ * created by doyoon
+ * MyGPAdto와 같음
+ * Settingdto 는 따로 만들어야함
+ */
+
+public class GPAdto {
+	private int id; // auto increment id
+	private int year; // 학년
+	private int semester; // 학기
+	private int credit; // 학점
+	private String grade; // A B+ C-
+	private String major; // 전공 교양 ///// 전공이면 true 교양이면 false
+	private String subject; // 과목명
+
+	public GPAdto() {
+		super();
+	}
+
+	public GPAdto(int id, int year, int semester, int credit, String grade,
+			String major, String subject) {
+		super();
+		this.id = id;
+		this.year = year;
+		this.semester = semester;
+		this.credit = credit;
+		this.grade = grade;
+		this.major = major;
+		this.subject = subject;
+	}
+
+	public GPAdto(int year, int semester, int credit, String grade,
+			String major, String subject) {
+		super();
+		this.year = year;
+		this.semester = semester;
+		this.credit = credit;
+		this.grade = grade;
+		this.major = major;
+		this.subject = subject;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId() {
+		// /
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getSemester() {
+		return semester;
+	}
+
+	public void setSemester(int semester) {
+		this.semester = semester;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("MyGPAdto [id=");
+		builder.append(id);
+		builder.append(", year=");
+		builder.append(year);
+		builder.append(", semester=");
+		builder.append(semester);
+		builder.append(", credit=");
+		builder.append(credit);
+		builder.append(", grade=");
+		builder.append(grade);
+		builder.append(", major=");
+		builder.append(major);
+		builder.append(", subject=");
+		builder.append(subject);
+		builder.append("]");
+		return builder.toString();
+	}
+}
