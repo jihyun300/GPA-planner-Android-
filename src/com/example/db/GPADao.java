074d4eeaace79e52f21dd07e611b47730e5f3eed
dohyoon.kim131@gmail.com <dohyoon.kim131@gmail.com@56f0695f-1845-8957-950b-df383e6b9fbd>
2014-02-07 06:16:16 +0000

    doyoon 2/7/15:16 DAO 수정

--- src/com/example/db/GPADao.java
+++ src/com/example/db/GPADao.java
@@ -1,4 +1,5 @@
 package com.example.db;
+
 /*
  * created by doyoon
  * GPAdao.java 파일과 같음
@@ -12,26 +13,34 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-public class  GPADao {
+public class GPADao {
 
-	public  GPADbHelper dbHelper;
+	public GPADbHelper dbHelper;
 	public SQLiteDatabase db;
 
-	public  GPADao(Context context) {
-		dbHelper = new  GPADbHelper(context);
+	public GPADao(Context context) {
+		dbHelper = new GPADbHelper(context);
 		db = dbHelper.getWritableDatabase();
 	}
 
 	// 전체
-	public List< GPADto> getAllList() {
-		List< GPADto> dtoList = new ArrayList< GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+  GPADbHelper.TABLE_NAME  +";", null);
+	public List<GPADto> getAllList() {
+		List<GPADto> dtoList = new ArrayList<GPADto>();
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME + ";", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new  GPADto(cs.getInt(0),cs.getInt(1), cs.getInt(2), cs.getInt(3),
-					cs.getString(4), cs.getString(5), cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
@@ -40,138 +49,213 @@ public class  GPADao {
 	// 1-1 성적
 	public List<GPADto> getList_1_1() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '1%' AND semester like '1%' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME
+					+ " WHERE year like '1%' AND semester like '1%' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0) ,cs.getInt(1), cs.getInt(2), cs.getInt(3),
-					cs.getString(4), cs.getString(5), cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
+
 	// 1-2 성적
 	public List<GPADto> getList_1_2() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '1 %' AND semester like '2 %' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME
+					+ " WHERE year like '1 %' AND semester like '2 %' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1), cs.getInt(2), cs.getInt(3),
-					cs.getString(4), cs.getString(5), cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
+
 	// 2-1 성적
 	public List<GPADto> getList_2_1() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '2 %' AND semester like '1 %' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME
+					+ " WHERE year like '2 %' AND semester like '1 %' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0) ,cs.getInt(1), cs.getInt(2), cs.getInt(3),
-					cs.getString(4), cs.getString(5), cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
+
 	// 2-2 성적
 	public List<GPADto> getList_2_2() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '"+"2 "+"%' AND semester like '2 %' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME + " WHERE year like '" + "2 "
+					+ "%' AND semester like '2 %' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1), cs.getInt(2),
-					cs.getInt(3), cs.getString(4), cs.getString(5),cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
+
 	// 3-1 성적
 	public List<GPADto> getList_3_1() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '3 %' AND semester like '1 %' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME
+					+ " WHERE year like '3 %' AND semester like '1 %' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1), cs.getInt(2),
-					cs.getInt(3), cs.getString(4), cs.getString(5),cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
-	//3-2 성적
+
+	// 3-2 성적
 	public List<GPADto> getList_3_2() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '3 %' AND semester like '2 %' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME
+					+ " WHERE year like '3 %' AND semester like '2 %' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1), cs.getInt(2),
-					cs.getInt(3), cs.getString(4), cs.getString(5),cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
-	//4-1 성적
+
+	// 4-1 성적
 	public List<GPADto> getList_4_1() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '4 %' AND semester like '1 %' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME
+					+ " WHERE year like '4 %' AND semester like '1 %' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1), cs.getInt(2),
-					cs.getInt(3), cs.getString(4), cs.getString(5),cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
-	//4-2 성적
+
+	// 4-2 성적
 	public List<GPADto> getList_4_2() {
 		List<GPADto> dtoList = new ArrayList<GPADto>();
-		Cursor cs = db.rawQuery("SELECT id, year, semester,"
-				+ "credit, grade, major, subject FROM"
-				+ GPADbHelper.TABLE_NAME
-				+ " WHERE year like '4 %' AND semester like '2 %' ;", null);
+		try {
+			Cursor cs = db.rawQuery("SELECT id, year, semester,"
+					+ "credit, grade, major, subject FROM"
+					+ GPADbHelper.TABLE_NAME
+					+ " WHERE year like '4 %' AND semester like '2 %' ;", null);
 
-		while (cs.moveToNext()) {
-			dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1), cs.getInt(2),
-					cs.getInt(3), cs.getString(4), cs.getString(5),cs.getString(6)));
+			while (cs.moveToNext()) {
+				dtoList.add(new GPADto(cs.getInt(0), cs.getInt(1),
+						cs.getInt(2), cs.getInt(3), cs.getString(4), cs
+								.getString(5), cs.getString(6)));
+			}
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
 		}
 
 		return dtoList;
 	}
 
-	
-	
-	
-	// INSERT 하는 로직  지현이꺼에 쓰일 로직
+	// INSERT 하는 로직 지현이꺼에 쓰일 로직
 	public void insertOneGPA(Object obj) {
 		GPADto dto = (GPADto) obj;
-		db.execSQL("INSERT INTO " + GPADbHelper.TABLE_NAME + "VALUES ("+null
-				+ dto.getYear() + "," + dto.getSemester() + ","
-				+ dto.getCredit() + ",'" + dto.getGrade() + "'," + "'"
-				+ dto.getMajor() + "'" + dto.getSubject() + "' );");
+		try {
+			db.execSQL("INSERT INTO " + GPADbHelper.TABLE_NAME + "VALUES ("
+					+ null + dto.getYear() + "," + dto.getSemester() + ","
+					+ dto.getCredit() + ",'" + dto.getGrade() + "'," + "'"
+					+ dto.getMajor() + "'" + dto.getSubject() + "' );");
+		} catch (Exception e) {
+			System.out.println("SQL오류");
+			e.printStackTrace();
+		} finally {
+			db.close();
+		}
 	}
-	
-//MainView에 보여져야함
-	//UPDATE
-	
+
+	// MainView에 보여져야함
+	// UPDATE
+
 	//
 }
