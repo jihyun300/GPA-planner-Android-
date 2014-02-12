91393db756fbc39bcf7e3aeca224b56c97765f25
dohyoon.kim131@gmail.com <dohyoon.kim131@gmail.com@56f0695f-1845-8957-950b-df383e6b9fbd>
2014-02-12 09:00:56 +0000

    SettingDao 수정

--- src/com/example/db/SettingDao.java
+++ src/com/example/db/SettingDao.java
@@ -7,25 +7,37 @@ import android.database.sqlite.SQLiteDatabase;
 public class SettingDao {
 	public SettingDbHelper dbHelper;
 	public SQLiteDatabase db;
-	
-	public SettingDao(Context context){
+
+	public SettingDao(Context context) {
 		dbHelper = new SettingDbHelper(context);
 		db = dbHelper.getWritableDatabase();
 	}
-	
-	public SettingDto getSettingInfo(){
-		Cursor cs = db.rawQuery("SELECT * FROM setting_table LIMIT 1;",null);
+
+	public SettingDto getSettingInfo() {
+		Cursor cs = db.rawQuery("SELECT * FROM setting_table LIMIT 1;", null);
 		SettingDto settingDto = null;
-		do{
-			settingDto = new SettingDto(cs.getInt(0),cs.getInt(1),cs.getFloat(2),cs.getInt(3));
-		}while(!cs.isLast());
-		return settingDto; 
+		try {
+			do {
+				settingDto = new SettingDto(cs.getInt(0), cs.getInt(1),
+						cs.getFloat(2), cs.getInt(3));
+			} while (!cs.isLast());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("SQL에러");
+		}finally{
+			if(db!=null){
+				db.close();
+			}
+			dbHelper.close();
+		}
+		return settingDto;
 	}
-	
-	public void updateSetting(){
+
+	public void updateSetting() {
 		//
 	}
-	public void deleteSetting(){
+
+	public void deleteSetting() {
 		//
 	}
 }
