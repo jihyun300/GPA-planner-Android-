6dd54ea60024233587451c70127f34257ff4884f
dhp0102@gmail.com <dhp0102@gmail.com@56f0695f-1845-8957-950b-df383e6b9fbd>
2014-02-20 06:58:38 +0000

    2014-02-20 오후 3:57 From 대현

--- src/com/example/gpaplan_mainpage/EditSubjectActivity.java
+++ src/com/example/gpaplan_mainpage/EditSubjectActivity.java
@@ -4,30 +4,60 @@
 package com.example.gpaplan_mainpage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.db.GPADto;
+import com.example.service.GPAService;
 
 /**
  * @author User
  *
  */
 public class EditSubjectActivity extends Activity {
-
+	GPAService	 gservice;
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		View footView = getLayoutInflater().inflate(R.layout.button_view, null);
-		Button add_button = (Button) footView.findViewById(R.id.addButton);
-		Button del_button = (Button) footView.findViewById(R.id.delButton);
-		add_button.setVisibility(View.INVISIBLE);
-		del_button.setVisibility(View.INVISIBLE);
-		this.setTitle("EditSubjectActivity");
-		setContentView(footView);	
+		// id를 넘겨줘서 DB로 접근.
+		Intent intent = getIntent();
+		int DBid=0;
+		intent.getIntExtra("DBid", DBid);		
+		gservice = new GPAService(getApplicationContext());
+		GPADto dto_temp = gservice.gpaDao.getDtoById(DBid);
+		setContentView(R.layout.item_view);
 		
+		Button del_button = (Button) findViewById(R.id.delButton);
+		del_button.setVisibility(View.INVISIBLE);
+		EditText edit_subject = (EditText)findViewById(R.id.editSubject);
+		edit_subject.setText(dto_temp.getSubject());
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		// TODO Auto-generated method stub
+		switch(item.getItemId()){
+		case R.id.action_ok1 :
+			//버튼누를시 수행할 명령문
+			return true;
+		
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.display_add, menu);
+		return super.onCreateOptionsMenu(menu);
+		
+	}
 }
