0d5d7c83122d85c5f67e99d67ce59ff6979469fc
dhp0102@gmail.com <dhp0102@gmail.com@56f0695f-1845-8957-950b-df383e6b9fbd>
2014-06-29 06:26:17 +0000

    2014/06/29 SettingsActivity를 또 수정함 1. 문제가 되는부분은 4.3->4.5로 전환할때 경고창에서 NO를 눌러도 4.5로 바뀌게 되는현상. 이는 AlertDialog.Builder로 만들기에는 한계가있었다. 위의방법은 onclick()메서드가 inner클래스로 구현되므로 외부에있는(Activity에 존재하는 변수들)에대해서 접근이 불가능했다. final로 선언하면 가능하기는 하나 이를 피하기위해서 DialogFragment를 커스터마이징해서 MyAlertDialogFragment를 만들엇다. 이를통해 Activity에 존재하는 메서드 doPositivieClick(), doNegativeClick()으로 접근하므로 외부변수들에 접근가능하다.
    2. '데이터 삭제'기능을 완성했다. controller객체를 통해 DB에 접근해서 DB 내용을 전부 삭제시켰다.
    이를위해 
    GPADao.java -> DeleteAllGpa() method
    GPAService.java -> DeleteGpaDb() method
    Controller.java -> DeleteGPaDb() method
    를 추가 시켯다.
    기능은 단순히 DB를 날리는 쿼리문을 수행한다.
    '데이터 삭제'라는 용어를 '데이터 초기화'로 수정했다.
    
    따라서 남은부분은
    1) 4.3->4.5 바꾸는거 (doPositiveClick메소드만 구현하면됨 70%)
    2) 설정창에서 모든 데이터 삭제하는 기능 - 내가할거(100%)
    3) 추가창에서 과목명 입력하면 텍스트가 안나오다가 다시한번누르면 텍스트가 나오는 현상(0%)
    1,3번을 해결해야할 듯 하다.
    5번문제는 6.28일 지현이와 얘기해봤고 이것 역시 에러임을 확인하였다.
    
    1번은 UPDATE gpa ...WHREE gpa='A-'와 같은 식으로 수정해주면 될거 같다고 생각한다.
    과목편집페이지에서 짯던 과정들을 되새겨보며 하면 될것 같다.
    
    ps.
    추가로 "내보내기" 기능을 만들어 보는 것도 나쁘지 않을거 같다.
    단순히 DB내용을 Sort해서 xls나 txt와 같은 포맷으로 내보내는것도 좋고
    (이경우에는 SD카드에 저장이 될거 같은데;; 그게 더 힘들어보인다.)
    Facebook이나 Twitter에 업로드가능한 이미지나 웹에서 참조가능한 포맷으로
    변환하는것도 괜찮을것 같다.

--- src/com/example/gpaplan_mainpage/ResetDialogPref.java
+++ src/com/example/gpaplan_mainpage/ResetDialogPref.java
@@ -1,5 +1,7 @@
 package com.example.gpaplan_mainpage;
 
+import com.example.controller.Controller;
+
 import android.content.Context;  
 import android.content.DialogInterface;  
 import android.content.SharedPreferences;  
@@ -10,10 +12,12 @@ import android.widget.Toast;
 
 public class ResetDialogPref extends DialogPreference {
 	protected Context context;
+	protected Controller controller;
 	public ResetDialogPref(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
 		this.context= context;
+		controller = new Controller(context);
 	}
 	@Override
 	public void onClick(DialogInterface dialog, int which) {
@@ -21,7 +25,16 @@ public class ResetDialogPref extends DialogPreference {
 		super.onClick(dialog, which);
 		if(which == DialogInterface.BUTTON_POSITIVE)
 		{
-			Toast.makeText(context,"OK button Pussed", Toast.LENGTH_SHORT).show();
+			try{
+			controller.DeleteGpaDb();
+			}
+			catch(Exception e){
+				Toast.makeText(context,"DB삭제에서 오류 발생!", Toast.LENGTH_SHORT).show();
+				e.printStackTrace();
+			}finally{
+				Toast.makeText(context,"DB의 내용이 전부 삭제되었습니다.", Toast.LENGTH_SHORT).show();	
+			}
+			
 		}
 	}
 }
